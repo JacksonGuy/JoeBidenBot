0ed6ecaf455894c9e2cfa455e78db5a8fcedc2f1
Jackson Frey <jacksonf955@gmail.com>
2021-10-20 22:22:14 -0400

    Update deploy_commands.js

--- deploy_commands.js
+++ deploy_commands.js
@@ -37,50 +37,6 @@ const commands = [
                 .setName("user")
                 .setDescription("User")
                 .setRequired(true)),
-                
-    new SlashCommandBuilder()
-        .setName("new")
-        .setDescription("Creates a new character"),
-
-    new SlashCommandBuilder()
-        .setName("move")
-        .setDescription("Moves player in a direction")
-        .addStringOption(option =>
-            option
-                .setName("direction")
-                .setDescription("Direction to move in")
-                .setRequired(true)),
-
-    new SlashCommandBuilder()
-        .setName("inspect")
-        .setDescription("Inspects a player character")
-        .addStringOption(option => 
-            option
-                .setName("player")
-                .setDescription("Player to inspect")
-                .setRequired(true)),
-    
-    new SlashCommandBuilder()
-        .setName("inventory")
-        .setDescription("Shows player inventory"),
-
-    new SlashCommandBuilder()
-        .setName("location")
-        .setDescription("View current location"),
-
-    new SlashCommandBuilder()
-        .setName("cast")
-        .setDescription("Casts a spell")
-        .addStringOption(option => 
-            option
-                .setName("spell")
-                .setDescription("Spell to cast")
-                .setRequired(true))
-        .addStringOption(option => 
-            option
-                .setName("target")
-                .setDescription("Enemy to cast spell on")
-                .setRequired(true)),
 
 ].map(command => command.toJSON());
 

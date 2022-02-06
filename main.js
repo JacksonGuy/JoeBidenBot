9346fc4ad5ce7d1dc1e846ed9baf2e9f81146f68
JacksonGuy <jacksonf955@gmail.com>
2022-02-05 19:39:21 -0500

    complete rework

--- main.js
+++ main.js
@@ -2,37 +2,76 @@ const { Client, Intents } = require('discord.js');
 const { token } = require("./config.json");
 const fs = require('fs');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+let leagueServers = require("./leagueServers.json");
+
+const botIntents = new Intents();
+botIntents.add(
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_PRESENCES, 
+    Intents.FLAGS.GUILD_MEMBERS);
+
+const client = new Client({ intents: botIntents });
+
+const prefix = "$";
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
     client.user.setActivity('im back losers', { type: 'PLAYING' });
 });
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
+async function checkLeague() {
+    for (server in leagueServers) {
+        client.guilds.fetch(server).then(guild => {
+            guild.fetch().then(guildData => {
+                guildData.members.fetch().then(members => {
+                    members.forEach(member => {
+                        const game = member.presence;
+                        if (game !== null && game.activities.length > 0) {
+                            const gamer = game.activities[0].name;
+                            if (gamer.toLowerCase() === 'league of legends') {
+                                client.channels.fetch(leagueServers[server]).then(channel => {
+                                    channel.send(`${member.user} is an idiot league player IMAGINE`);
+                                });
+                            }
+                        }
+                    });
+                });
+            });
+        });
+    }
+}
+setInterval(checkLeague, 300000);
 
-    switch(interaction.commandName) {
+client.on('messageCreate', async interaction => {
+    if (interaction.author.bot) return;
+    if (!interaction.content.startsWith(prefix)) return;
+
+    const commandBody = interaction.content.slice(prefix.length);
+    const args = commandBody.split(" ");
+    const command = args.shift().toLowerCase();
+
+    switch(command) {
         case 'test':
-            await interaction.reply("oh boy hopefully everything works");
+            checkLeague();
             break;
 
         case 'erase':
-            interaction.channel.messages.fetch({limit: 100 }).then(messages => {
+            amount = args[0];
+            interaction.channel.messages.fetch({limit: amount }).then(messages => {
                 messages.forEach(message => message.delete());
             });
-            await interaction.reply(`Deleting ${interaction.options.getInteger('amount')} messages`);
+            await interaction.channel.send(`Deleting ${amount} messages`);
             break;
         
-        case 'spam':
-            await interaction.reply(`${interaction.options.getString('message')}`);
-            for (let i = 0; i < interaction.options.getInteger('amount')-1; i++) {
-                await interaction.channel.send(`${interaction.options.getString('message')}`);
+        case 'spam': // DOES NOT WORK PLS FIXERINO
+            for (let i = 0; i < args[1]; i++) {
+                await interaction.channel.send(`${args[0]}`);
             }
             break;
 
         case 'stalin':
-            target = interaction.options.getString('user');
+            target = args[0];
             target = target.slice(3, target.length-1);
 
             interaction.channel.messages.fetch({limit: 100 }).then(messages => {
@@ -42,8 +81,26 @@ client.on('interactionCreate', async interaction => {
                     }
                 });
             });
-            await interaction.reply(`Deleting messages from ${interaction.options.getString('user')}`);
+            await interaction.channel.send(`Deleting messages from ${args[0]}`);
             break;
-});
+
+        case 'leaguebad':
+            if (args[0].toLowerCase() === 'on') {
+                leagueServers[interaction.guild.id] = interaction.channelId;
+                fs.writeFile('./leagueServers.json', JSON.stringify(leagueServers), (err) => {
+                    if (err) return console.log(err);
+                    console.log("Wrote to LeagueServers.json");
+                });
+            }
+            else if (args[0].toLowerCase() === 'off') {
+                leagueServers[interaction.guild.id] = '';
+                fs.writeFile('./leagueServers.json', JSON.stringify(leagueServers), (err) => {
+                    if (err) return console.log(err);
+                    console.log("Wrote to LeagueServers.json");
+                });
+            }
+            break;
+    }
+}); 
 
 client.login(token);

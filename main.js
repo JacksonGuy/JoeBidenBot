f8c0bc7e93721e529f0178b4305f7380a17c875c
Jackson Frey <jacksonf955@gmail.com>
2022-11-14 15:22:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection, Events } = require('discord.js');
 const { token } = require("./config.json");
-const fs = require('fs');
+const fs = require('node:fs');
+const path = require('node:path');
 
-let leagueServers = require("./leagueServers.json");
 
 const client = new Client({ intents: [
 	GatewayIntentBits.Guilds,
@@ -11,94 +11,42 @@ const client = new Client({ intents: [
 	GatewayIntentBits.GuildMembers,
 ]});
 
-const prefix = "$";
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
     client.user.setActivity('im back losers', { type: 'PLAYING' });
 });
 
-async function checkLeague() {
-    for (server in leagueServers) {
-        client.guilds.fetch(server).then(guild => {
-            guild.fetch().then(guildData => {
-                guildData.members.fetch().then(members => {
-                    members.forEach(member => {
-                        const game = member.presence;
-                        if (game !== null && game.activities.length > 0) {
-                            const gamer = game.activities[0].name;
-                            if (gamer.toLowerCase() === 'league of legends') {
-                                client.channels.fetch(leagueServers[server]).then(channel => {
-                                    channel.send(`${member.user} is an idiot league player IMAGINE`);
-                                });
-                            }
-                        }
-                    });
-                });
-            });
-        });
-    }
-}
-setInterval(checkLeague, 300000);
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
 
-client.on('messageCreate', async interaction => {
-    if (interaction.author.bot) return;
-    if (!interaction.content.startsWith(prefix)) return;
+	const command = interaction.client.commands.get(interaction.commandName);
 
-    const commandBody = interaction.content.slice(prefix.length);
-    const args = commandBody.split(" ");
-    const command = args.shift().toLowerCase();
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
 
-    switch(command) {
-        case 'test':
-            checkLeague();
-            break;
-
-        case 'erase':
-            amount = args[0];
-            interaction.channel.messages.fetch({limit: amount }).then(messages => {
-                messages.forEach(message => message.delete());
-            });
-            await interaction.channel.send(`Deleting ${amount} messages`);
-            break;
-        
-        case 'spam': // DOES NOT WORK PLS FIXERINO
-            for (let i = 0; i < args[1]; i++) {
-                await interaction.channel.send(`${args[0]}`);
-            }
-            break;
-
-        case 'stalin':
-            target = args[0];
-            target = target.slice(3, target.length-1);
-
-            interaction.channel.messages.fetch({limit: 100 }).then(messages => {
-                messages.forEach(message => {
-                    if (message.author.id === target) {
-                        message.delete();
-                    }
-                });
-            });
-            await interaction.channel.send(`Deleting messages from ${args[0]}`);
-            break;
-
-        case 'leaguebad':
-            if (args[0].toLowerCase() === 'on') {
-                leagueServers[interaction.guild.id] = interaction.channelId;
-                fs.writeFile('./leagueServers.json', JSON.stringify(leagueServers), (err) => {
-                    if (err) return console.log(err);
-                    console.log("Wrote to LeagueServers.json");
-                });
-            }
-            else if (args[0].toLowerCase() === 'off') {
-                leagueServers[interaction.guild.id] = '';
-                fs.writeFile('./leagueServers.json', JSON.stringify(leagueServers), (err) => {
-                    if (err) return console.log(err);
-                    console.log("Wrote to LeagueServers.json");
-                });
-            }
-            break;
-    }
-}); 
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
 
 client.login(token);

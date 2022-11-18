62028c095380b67232dea21bb51fa602267d840f
JacksonGuy <jacksonf955@gmail.com>
2022-11-18 01:31:09 -0500

    baltop update wip

--- commands/bal.js
+++ commands/bal.js
@@ -14,11 +14,11 @@ module.exports = {
         async execute(interaction) {
             let server = interaction.guild;
             let author = interaction.user;
-            fs.readFile('./data/player_item_data.json', (err, data1) => {
-                if (err) throw err;
+            fs.readFile('./data/player_time_data.json', (err1, data1) => {
+                if (err1) throw err1;
                 let time_data = JSON.parse(data1);
-                fs.readFile('./data/player_item_data.json', (err, data2) => {
-                    if (err) throw err;
+                fs.readFile('./data/player_item_data.json', (err2, data2) => {
+                    if (err2) throw err2;
                     let player_items = JSON.parse(data2);
                     let last_time = time_data[server.id][author.id];
                     let time = Math.floor( (Date.now() - last_time) / 1000); // in seconds
@@ -26,11 +26,15 @@ module.exports = {
                     // Update player income
                     for (let item in player_items[server.id][author.id]) {
                         // $$ per item * amount of that item * amount of time
-                        income += (item_data[item]["income"] * player_items[item]) * time
+                        income += (item_data[item]["income"] * player_items[server.id][user][item]) * time
                     }
+                    // Update player time
+                    time_data[server.id][author.id] = Date.now();
+                    time_data = JSON.stringify(time_data, null, 2);
+                    fs.writeFileSync('./data/player_time_data.json', time_data);
 
-                    fs.readFile('./data/balance_data.json', (err, data3) => {
-                        if (err) throw err;
+                    fs.readFile('./data/balance_data.json', (err3, data3) => {
+                        if (err3) throw err3;
                         let bal_data = JSON.parse(data3);
                         bal_data[server.id][author.id] += income;
                         let bal = bal_data[server.id][author.id];
--- commands/baltop.js
+++ commands/baltop.js
@@ -12,16 +12,36 @@ module.exports = {
         .setName("baltop")
         .setDescription("Shows the top balances of users in the server"),
         async execute(interaction) {
-            var bal_data;
+            let bal_data;
+            let server = interaction.guild;
+            fs.readFile('./data/player_time_data.json', (err1, data1) => {
+                if (err1) throw err1;
+                let time_data = JSON.parse(data1);
+                fs.readFile('./data/player_item_data.json', (err2, data2) => {
+                    if (err2) throw err2;
+                    let player_items = JSON.parse(data2);
+                    fs.readFile('./data/balance_data.json', (err3, data3) => {
+                        if (err3) throw err3;
+                        bal_data = JSON.parse(data3);
+                        for (let user in bal_data[server.id]) {
+                            let last_time = time_data[server.id][user];
+                            let time = Math.floor( (Date.now() - last_time) / 1000); // in seconds
+                            let income = 1;
+                            for (let item in player_items[server.id][user]) {
+                                income += (item_data[item]["income"] * player_items[server.id][user][item]) * time;
+                            }
+                            // Update player time
+                            time_data[server.id][user] = Date.now();
+                        }
+                    })
+                })
+            })
             fs.readFile('./data/balance_data.json', (err, data) => {
                 if (err) throw err;
                 bal_data = JSON.parse(data);
-                for (var server in bal_data) {
-                    if (server != interaction.guild.id) return;
-                    for (var user in bal_data[server]) {
-                        interaction.guild.members.fetch(user)
-                            .then((person) => interaction.channel.send(`${person.displayName}: ${bal_data[server][user]}`));
-                    }
+                for (let user in bal_data[server.id]) {
+                    interaction.guild.members.fetch(user)
+                        .then((person) => interaction.channel.send(`${person.displayName}: ${bal_data[server][user]}`));
                 }
                 interaction.reply("Top balances for this server: ");
             });

6f79af6c16da00dade259310d57e9064ff204153
JacksonGuy <jacksonf955@gmail.com>
2022-11-19 14:11:21 -0500

    fixed issues with buy command change the amount parameter to string removed max parameter from buy, now you just specify in amount

--- commands/buy.js
+++ commands/buy.js
@@ -1,21 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
 const fs = require("fs");
 
-var bal_data;
 var item_data;
-var player_items;
-
-fs.readFile('./data/balance_data.json', (err, data) => {
-    if (err) throw err;
-    bal_data = JSON.parse(data);
-});
 fs.readFile('./data/item_data.json', (err, data) => {
     if (err) throw err;
     item_data = JSON.parse(data);
-});
-fs.readFile("./data/player_item_data.json", (err, data) => {
-    if (err) throw err;
-    player_items = JSON.parse(data);
 });
 
 module.exports = {
@@ -27,46 +16,54 @@ module.exports = {
                 .setName("item")
                 .setDescription("The item to buy")
                 .setRequired(true))
-        .addIntegerOption(option =>
-            option
-                .setName("amount")
-                .setDescription("The amount of the item to buy"))
         .addStringOption(option =>
             option
-                .setName("max")
-                .setDescription("Buy maximum amount of the item")),
+                .setName("amount")
+                .setDescription("The amount of the item to buy (put 0 to buy maximum amount)")),
         async execute(interaction) {
             let item = interaction.options.getString("item");
-            let item_cost = item_data[item]["price"];
-
             let server = interaction.guild;
             let author = interaction.user;
-            let bal = bal_data[server.id][author.id];
 
-            if (!interaction.options.getString("max")) {
-                let amount = 1;
-                if (interaction.options.getInteger("amount")) {
-                    amount = interaction.options.getInteger("amount");
-                }
-                if (amount * item_cost <= bal) {
-                    player_items[server.id][author.id][item] += amount;
-                    bal_data[server.id][author.id] -= (amount * item_cost);
-                    await interaction.reply(`${amount} ${item} purchased`);
-                }
-                else {
-                    await interaction.reply("You can't afford that");
-                }
-            }
-            else {
-                let amount = Math.floor(bal / item_cost);
-                player_items[server.id][author.id][item] += amount;
-                bal_data[server.id][author.id] -= (amount * item_cost);
-                await interaction.reply(`${amount} ${item} purchased`);
+            if (!(item in item_data)) {
+                await interaction.reply("Item doesn't exist");
+                return;
             }
 
-            bal_data = JSON.stringify(bal_data, null, 2);
-            fs.writeFileSync('./data/balance_data.json', bal_data)
-            player_items = JSON.stringify(player_items, null, 2);
-            fs.writeFileSync('./data/player_item_data.json', player_items);
+            fs.readFile('./data/balance_data.json', (err1, data1) => {
+                if (err1) throw err1;
+                let bal_data = JSON.parse(data1);
+                fs.readFile('./data/player_item_data.json', (err2, data2) => {
+                    if (err2) throw err2;
+                    let player_items = JSON.parse(data2);
+                    let item_cost = item_data[item]["price"];
+                    let bal = bal_data[server.id][author.id];
+
+                    let amount = 1;
+                    if (interaction.options.getString("amount")) {
+                        amount = parseInt(interaction.options.getString("amount"));
+                    }
+                    if (amount === 0) { // Buy max
+                        amount = Math.floor(bal / item_cost);
+                        player_items[server.id][author.id][item] += amount;
+                        bal_data[server.id][author.id] -= (amount * item_cost);
+                        interaction.reply(`${amount} ${item} purchased`);
+                    }
+                    else { // Buy amount
+                        if (amount * item_cost <= bal) {
+                            player_items[server.id][author.id][item] += amount;
+                            bal_data[server.id][author.id] -= (amount * item_cost);
+                            interaction.reply(`${amount} ${item} purchased`);
+                        }
+                        else {
+                            interaction.reply("You can't afford that");
+                        }
+                    }
+                    bal_data = JSON.stringify(bal_data, null, 2);
+                    fs.writeFileSync('./data/balance_data.json', bal_data)
+                    player_items = JSON.stringify(player_items, null, 2);
+                    fs.writeFileSync('./data/player_item_data.json', player_items);
+                });
+            });
         }
 }
--- commands/items.js
+++ commands/items.js
@@ -11,7 +11,6 @@ module.exports = {
             fs.readFile('./data/player_item_data.json', (err, data) => {
                 if (err) throw err;
                 let item_data = JSON.parse(data);
-                console.log(item_data);
                 for (let item in item_data[server.id][author.id]) {
                     interaction.channel.send(`${item}: ${item_data[server.id][author.id][item]}`);
                 }

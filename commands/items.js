a30286b5d9c1e3f5e64a7de8e94f09bc4594d13e
JacksonGuy <jacksonf955@gmail.com>
2022-11-20 02:44:31 -0500

    changed items to list shop items replaced by myitems command

--- commands/items.js
+++ commands/items.js
@@ -1,20 +1,20 @@
 const { SlashCommandBuilder } = require('discord.js');
 const fs = require("fs");
 
+var items;
+fs.readFile('./data/item_data.json', (err, data) => {
+    if (err) throw err;
+    items = JSON.parse(data);
+});
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("items")
-        .setDescription("Lists the user's items"),
+        .setDescription("Lists the items available to buy"),
         async execute(interaction) {
-            let server = interaction.guild;
-            let author = interaction.user;
-            fs.readFile('./data/player_item_data.json', (err, data) => {
-                if (err) throw err;
-                let item_data = JSON.parse(data);
-                for (let item in item_data[server.id][author.id]) {
-                    interaction.channel.send(`${item}: ${item_data[server.id][author.id][item]}`);
-                }
-            });
-            interaction.reply("Your items: ");
+            for (let item in items) {
+                interaction.channel.send(`${item}: $${items[item]["price"]}`);
+            }
+            interaction.reply("Store: ");
         }
-}
+    }
---
+++ commands/myitems.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require("fs");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("myitems")
+        .setDescription("Lists the user's items"),
+        async execute(interaction) {
+            let server = interaction.guild;
+            let author = interaction.user;
+            fs.readFile('./data/player_item_data.json', (err, data) => {
+                if (err) throw err;
+                let item_data = JSON.parse(data);
+                for (let item in item_data[server.id][author.id]) {
+                    interaction.channel.send(`${item}: ${item_data[server.id][author.id][item]}`);
+                }
+            });
+            interaction.reply("Your items: ");
+        }
+}
